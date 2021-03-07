67f4c06a0d7b8265f5ba8ab59b20dc3015536860
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-07 20:39:06 +0100

    fix: fix the task "Brackets"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayOfBrackets = Array.from(String(str));
+    let emptyArr = [];
+    let brConfCalc = 0;
+    for (let i = 0; i < arrayOfBrackets.length; i++) {
+        while (brConfCalc < bracketsConfig.length) {
+            if (arrayOfBrackets[i] === bracketsConfig[brConfCalc][0]) {
+                if (bracketsConfig[brConfCalc][0] === bracketsConfig[brConfCalc][1]) {
+                    if (emptyArr.length > 0 && emptyArr[emptyArr.length - 1] === bracketsConfig[brConfCalc][0]) {
+                        emptyArr.pop();
+                    } else {
+                        emptyArr.push(arrayOfBrackets[i]);
+                    }
+                } else {
+                    emptyArr.push(arrayOfBrackets[i]);
+                }
+            } else if (arrayOfBrackets[i] === bracketsConfig[brConfCalc][1]) {
+                let lastPop = emptyArr.pop();
+                if (bracketsConfig[brConfCalc][0] !== lastPop) {
+                    return false;
+                }
+            }
+            brConfCalc++;
+        }
+        brConfCalc = 0;
+    }
+    if (emptyArr.length > 0) {
+        return false
+    }
+    return true;
 }
